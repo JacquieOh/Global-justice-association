284b9b8c6d39e6a16a3ebdba0118a6cb09fd7a9e
JacquieOh <69905851+JacquieOh@users.noreply.github.com>
2021-01-22 13:15:22 +0100

    added npm packages

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const app = express();
 
+
+app.get('/', function (req, res) {
+  res.send('Hello World, it is Friday and the sun is shining')
+});
+
+// app.get('/contact', function (req, res) {
+//   res.send('You can contact us on info@global-justice.com')
+// });
+
+app.listen(3000, function (){
+  console.log('Server is running')
+});
