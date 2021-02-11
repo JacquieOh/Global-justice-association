0e49514967e9e0564b44fbcfadc7864449769cdf
JacquieOh <69905851+JacquieOh@users.noreply.github.com>
2021-02-11 11:16:37 +0100

    changes to contact form and handler

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+//message Vars
+
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$email_from = 'info@global-justice.com';
+$email_subject = "New Form Submission";
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                  "User Subject: $subject.\n".
+                  "User Message: $message.\n";
+
+$to = "siebkejacqueline@yahoo.de";
+$headers = "From: $email_from \r\n";
+$headers .="Reply-To: $visitor_email \r\n";
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: index.html");
+
+  ?>
